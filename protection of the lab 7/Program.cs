05673a03e971f25ddb18919b2e2c6baa1f51eb51
krasnova-mariana <m2312258@edu.misis.ru>
2024-04-16 22:57:17 +0300

    Protection of laboratory work 7

---
+++ protection of the lab 7/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_lab_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter kind of sport (1 - figure skating, 2 - speed skating):");
+            int kind = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter discipline name:");
+            string sportName = Console.ReadLine();
+            Wintersport[] a = new Wintersport[5];
+            string name;
+            double[] b = new double[7];
+            for (int i = 0; i < a.Length; i++)
+            {
+                name = Console.ReadLine();
+                for (int j = 0; j < 7; j++)
+                    b[j] = Convert.ToDouble(Console.ReadLine());
+                if (kind == 1)
+                    a[i] = new FigureSkating(name, b, sportName);
+                else
+                    a[i] = new SpeedSkating(name, b, sportName);
+            }
+            initPlaces(a);
+            for (int i = 0; i < a.Length; i++)
+                a[i].calcSum();
+            a = a[0].QuickSort(a, 0, a.Length - 1);
+            Console.WriteLine("Discipline Name: " + a[0].getDisciplinename());
+            //Console.WriteLine("Discipline Name: " + Wintersport.getDisciplinename());
+
+            for (int i = 0; i < a.Length; i++)
+                a[i].DisplayInfo();
+        }
+        static void initPlaces(Wintersport[] a)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                double[] b = new double[a.Length];
+                int[] p = new int[a.Length];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    b[i] = a[i].getBall(j);
+                    p[i] = i;
+                }
+                bubbleSort(b, p);
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[p[i]].setPlace(j, i + 1);       //a[p[i]].place[j] = i+1;
+                }
+            }
+        }
+        static void bubbleSort(double[] b, int[] p)
+        {
+            int tmp;
+            double tmpd;
+            for (int step = 1; step < b.Length; step++)
+                for (int i = b.Length - 1; i >= step; i--)
+                    if (b[i] > b[i - 1])
+                    {
+                        tmpd = b[i];
+                        b[i] = b[i - 1];
+                        b[i - 1] = tmpd;
+                        tmp = p[i];
+                        p[i] = p[i - 1];
+                        p[i - 1] = tmp;
+                    }
+        }
+    }
+    abstract class Wintersport
+    {
+        private string name;
+        private double[] ball;
+        private int[] place;
+        private int sum;
+        private string disciplinename;
+        //private static string disciplinename;
+        public Wintersport(string name, double[] b, string disciplinename)
+        {
+            this.name = name;
+            ball = new double[7];
+            place = new int[7];
+            for (int j = 0; j < 7; j++)
+                ball[j] = b[j];
+            sum = 0;
+            this.disciplinename = disciplinename;
+        }
+        public string getDisciplinename()
+        {
+            return disciplinename;
+        }
+        public double getBall(int j)
+        {
+            return ball[j];
+        }
+        public void setPlace(int j, int pl)
+        {
+            place[j] = pl;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine(name);
+            for (int j = 0; j < 7; j++)
+                Console.Write(ball[j] + " ");
+            Console.WriteLine();
+            for (int j = 0; j < 7; j++)
+                Console.Write(place[j] + " ");
+            Console.WriteLine();
+            Console.WriteLine("Sum: " + sum);
+            //Console.WriteLine("Discipline Name: " + disciplinename);
+        }
+        public void calcSum()
+        {
+            sum = 0;
+            for (int j = 0; j < 7; j++)
+                sum += place[j];
+        }
+        public int FindPivot(Wintersport[] a, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+            Wintersport tmp;
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (a[i].sum < a[maxIndex].sum)
+                {
+                    pivot++;
+                    tmp = a[pivot];
+                    a[pivot] = a[i];
+                    a[i] = tmp;
+                }
+            }
+
+            pivot++;
+            tmp = a[pivot];
+            a[pivot] = a[maxIndex];
+            a[maxIndex] = tmp;
+
+            return pivot;
+        }
+        public Wintersport[] QuickSort(Wintersport[] a, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return a;
+            }
+
+            int pivot = FindPivot(a, minIndex, maxIndex);
+            QuickSort(a, minIndex, pivot - 1);
+            QuickSort(a, pivot + 1, maxIndex);
+
+            return a;
+        }
+    }
+    class FigureSkating : Wintersport
+    {
+        public FigureSkating(string name, double[] b, string disciplinename) : base(name, b, disciplinename) { }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Figure Skating");
+            base.DisplayInfo();
+        }
+    }
+    class SpeedSkating : Wintersport
+    {
+        public SpeedSkating(string name, double[] b, string disciplinename) : base(name, b, disciplinename) { }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Speed Skating");
+            base.DisplayInfo();
+        }
+    }
+}
+
